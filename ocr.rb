bf1d0448db1dfabb847b2afc11d2bf531b30ce9c
Sidnei Pacheco <sidnei.junior@hubfintech.com.br>
2016-07-25 14:08:41 -0300

    ocr start

---
+++ ocr.rb
@@ -0,0 +1,15 @@
+# ocr.rb
+# encoding: utf-8
+require 'tesseract'
+
+engine = Tesseract::Engine.new do |config|
+  config.language  = :eng
+  config.blacklist = '|'
+end
+
+def clean(text)
+  text.split(/\n/).compact.select { |v| v.size > 0 }
+end
+
+puts clean(engine.text_for(ARGV.first))
+
